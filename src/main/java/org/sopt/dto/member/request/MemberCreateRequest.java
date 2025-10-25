8a709a6642baabb116710d8f6a6f9641d4f89c84
hyodongg <whgyehdjhd@naver.com>
2025-10-25 23:30:21 +0900

    feat: dto 생성

---
+++ src/main/java/org/sopt/dto/member/request/MemberCreateRequest.java
@@ -0,0 +1,10 @@
+package org.sopt.dto.member.request;
+
+import org.sopt.domain.Gender;
+
+public class MemberCreateRequest {
+    private String name;
+    private String birth;
+    private String email;
+    private Gender gender;
+}
---
+++ src/main/java/org/sopt/dto/member/response/MemberResponse.java
@@ -0,0 +1,11 @@
+package org.sopt.dto.member.response;
+
+import org.sopt.domain.Gender;
+
+public class MemberResponse {
+    private Long id;
+    private String name;
+    private String birth;
+    private String email;
+    private Gender gender;
+}
