01aeb7b45db49a317c227e44bd8b20e3d0b78c56
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2025-11-26 14:53:00 +0900

    feat: dto 검증 추가

--- src/main/java/org/sopt/member/dto/request/MemberCreateRequest.java
+++ src/main/java/org/sopt/member/dto/request/MemberCreateRequest.java
@@ -3,10 +3,11 @@ package org.sopt.member.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.sopt.member.entity.Gender;
 
 public record MemberCreateRequest(@NotBlank(message = "이름은 필수 입니다.") String name,
-                                  @NotBlank(message = "생년월일은 필수 입니다") String birth,
+                                  @NotBlank(message = "생년월일은 필수 입니다") @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "생년월일 형식이 올바르지 않습니다. yyyy-MM-dd 형식으로 입력해주세요.") String birth,
                                   @NotBlank(message = "이메일은 필수 입니다") @Email(message = "이메일 형식이 올바르지 않습니다") String email,
                                   @NotNull(message = "성별을 입력해주세요") Gender gender,
                                   String password) {
