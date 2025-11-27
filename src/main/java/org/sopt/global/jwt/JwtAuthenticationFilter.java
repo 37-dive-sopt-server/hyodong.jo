64e396926e4b7755242defc63e15c8bf8fc6d4b6
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2025-11-28 02:02:13 +0900

    feat: jwt 필터 구현

---
+++ src/main/java/org/sopt/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package org.sopt.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.sopt.global.jwt.exception.JwtException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.util.List;
+
+import java.io.IOException;
+
+
+/**
+ * JWT 인증 필터
+ * 모든 HTTP 요청마다 실행되어 JWT 토큰을 검증하고
+ * SecurityContext에 인증 정보를 저장
+ */
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws IOException, ServletException {
+
+        try{
+            String token = extractTokenFromHeader(request);
+
+            if(token != null) {
+                Long memberId = jwtService.getMemberIdFromToken(token);
+
+                if (!jwtService.isAccessToken(token)) {
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        memberId,
+                        null,
+                        List.of(new SimpleGrantedAuthority("ROLE_USER"))
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch(JwtException e){
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    // 헤더에서 JWT추출
+    private String extractTokenFromHeader(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            return null;
+        }
+
+        return authorization.substring(7);
+    }
+}
