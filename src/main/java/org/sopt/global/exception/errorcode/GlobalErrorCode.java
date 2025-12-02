f8ad51dbee7bbed4ccfeea25305074ac3d21ca55
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2025-12-02 17:56:23 +0900

    refactor: 에러코드 정리

--- src/main/java/org/sopt/global/exception/errorcode/GlobalErrorCode.java
+++ src/main/java/org/sopt/global/exception/errorcode/GlobalErrorCode.java
@@ -8,9 +8,12 @@ public enum GlobalErrorCode implements ErrorCode {
 
     // jwt 관련
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "J001", "유효하지 않은 토큰입니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "J002", "만료된 토큰입니다."),
-    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "J003", "토큰이 비어있습니다."),
-    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN,"J004","접근 권한이 없습니다.");
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "J002", "유효하지 않은 액세스 토큰입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "J003", "유효하지 않은 리프레쉬 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "J004", "만료된 토큰입니다."),
+    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "J005", "토큰이 비어있습니다."),
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN,"J006","접근 권한이 없습니다.");
+    ;
 
     private final HttpStatus status;
     private final String code;
